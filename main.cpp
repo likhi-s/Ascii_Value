3059110b2531e1d0b4200fa8b2c382f160cf3105
Likitha <likithasshivareddy@gmail.com>
2024-10-09 14:36:18 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+
+    char ch;
+    cout<<"Enter the Character"<<endl;
+    cin>>ch;
+    cout<<"ASCII Value of Character"<<" "<<ch<<" "<<"is:"<<int(ch)<<endl;
+
+    return 0;
+
+}
